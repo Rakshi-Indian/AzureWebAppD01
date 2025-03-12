b8e8d3abe56d7045b25915f88826c097a179c9cc
Rakshi-Indian <69146960+Rakshi-Indian@users.noreply.github.com>
2025-03-12 15:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette.responses import JSONResponse
@@ -9,7 +10,7 @@ security = HTTPBasic()
 def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     valid_username = "admin"
     valid_password = "password"
-    
+
     if credentials.username != valid_username or credentials.password != valid_password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -25,3 +26,6 @@ def read_root():
 @app.get("/secure-endpoint")
 def secure_endpoint(username: str = Depends(authenticate)):
     return {"message": f"Hello, {username}. You have accessed a secure endpoint."}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
